f0e448038ff77a38fc83ab0134ac7d459b131d9e
kirillpanovk-collab <kirillpanovk@gmail.com>
2026-07-31 18:49:02 +0300

    Обновить дизайн и иконку в цвете бургунди

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="ryadom-pages-v1";
+const VERSION="ryadom-pages-v2-burgundy";
 const BASE=new URL("./",self.location).pathname;
 const SHELL=[BASE,`${BASE}index.html`,`${BASE}styles.css`,`${BASE}app.js`,`${BASE}manifest.webmanifest`,`${BASE}icon-192.png`,`${BASE}icon-512.png`,`${BASE}apple-touch-icon.png`];
 self.addEventListener("install",event=>event.waitUntil(caches.open(VERSION).then(cache=>cache.addAll(SHELL))));
